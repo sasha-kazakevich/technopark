75fd26b3609ddd23722ad4821e8c16242610b6ea
Sasha Kazakevich <ssasshaka@gmail.com>
2015-08-10 17:07:47 +0300

    new

--- js/script.js
+++ js/script.js
@@ -2,7 +2,19 @@ $(document).ready(function(){
     $("select.type").selecter({
     	label: "любого типа"
     });
-     $("select.brand").selecter({
+    $("select.brand").selecter({
     	label: "любого бренда"
     });
-});
+    $("#img-menu").click(function(){
+    	$("#menu").toggle("fast");
+	});
+	$("#img-search").click(function(){
+     	$("#search").toggle("fast");
+	});
+	$("#img-filter").click(function(){
+		$(".selector").toggle("fast");
+	});
+
+ });
+
+
